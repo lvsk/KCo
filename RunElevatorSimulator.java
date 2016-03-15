f43ea8bb55a809d5fdecf9ab614b78fcc00aedb1
lvsk <lav.sivakumar@gmail.com>
2016-03-14 21:46:48 -0400

    Added files via upload
    Initial checkin

---
+++ RunElevatorSimulator.java
@@ -0,0 +1,11 @@
+package main.test;
+
+public class RunElevatorSimulator {
+	public static void main(String[] args) {		
+		//Initialize the elevator simulation with the desired number of elevators, and the desired number of floors.
+		//Assume ground/min of 1.
+		int groundFloor = 1;
+		
+		//Create Elevator objects, ElevatorUser objects, ElevatorController to handle user requests
+	}
+}
